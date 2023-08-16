9ef4cc2c081930c06b50126388b7455732063f92
Mariana Cuadrado <138075011+MarianaCuadrado@users.noreply.github.com>
2023-08-16 19:41:11 -0300

    Desarrollo del programa: importar clases, opciones agregar y listar peliculas

--- app.py
+++ app.py
@@ -1,21 +1,29 @@
 #importar clases del archivo clases.py
 from clases import *
 
+nombre_catalogo = input("¡BIENVENIDO!\nPor favor, ingrese el nombre del catálogo de películas: ")
+catalogo = CatalogoPelicula(nombre_catalogo)
+
 opcion = None
 
 while opcion != 4:
     print("Opciones:")
-    print("1. Agregar pelicula")
-    print("2. Listar pelicula")
-    print("3. Eliminar catalogo de peliculas")
+    print("1. Agregar película")
+    print("2. Listar película")
+    print("3. Eliminar catalogo de películas")
     print("4. Salir")
 
     opcion = int (input ("Por favor, ingresa tu opción del 1 al 4: "))
 
     if opcion == 1:
+        nombre_pelicula = input ("Por favor, ingrese el nombre de la pelicula: ")
+        pelicula = Pelicula(nombre_pelicula)
+        catalogo.agregar_pelicula(pelicula)
+
     elif opcion == 2:
-    elif opcion == 3:
-    elif opcion == 4:
-        print("Programa finalizado")
+        catalogo.listar_peliculas()
+
+    
+        
 
 
